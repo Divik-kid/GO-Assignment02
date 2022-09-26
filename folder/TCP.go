435e7392b61e963701a10659b44de0f08a22ded8
emno <emno@itu.dk>
2022-09-26 12:59:52 +0200

    implement sending multible messages
    This make the client able to send multible messages
    to the server, and make the server able to recive
    them

--- folder/TCP.go
+++ folder/TCP.go
@@ -20,17 +20,20 @@ func main() {
 func client(ch chan packet) {
 	seq := 0
 	ack := 0
-	for {
+
+	dataToSend := []string{"here", "is", "data"}
+
+	for index, element := range dataToSend {
 		//Step 1 send x = syn seq'
 		time.Sleep(1 * time.Second)
-		ch <- packet{"", seq, ack, 0} //
+		ch <- packet{"", seq, ack, 0, 0}
 		fmt.Println("sending step1")
 		//Step 2 recieve x+1 and y
 		synAck := <-ch
 		if synAck.acknowledgement == seq+1 && synAck.sequence == ack+100 {
 			fmt.Println("sending data")
 			//Step 3 send y+1 and x+1 and data
-			ch <- packet{"Here is my data", synAck.acknowledgement, synAck.sequence, synAck.orderNumber + 1}
+			ch <- packet{element, synAck.acknowledgement, synAck.sequence, index, len(dataToSend)}
 			seq++
 			ack = ack + 100
 		}
@@ -41,6 +44,8 @@ func server(ch chan packet) {
 	exSec := 0
 	exAck := 0
 
+	recivedData := make([]string, 0)
+
 	for {
 
 		//Step 1 listen for requests, check x = syn
@@ -48,7 +53,7 @@ func server(ch chan packet) {
 		fmt.Println("recieving step1")
 		if syn.sequence == exSec {
 			//Step 2 send, x+1 and y
-			ch <- packet{"", syn.acknowledgement + 100, syn.sequence + 1, 0}
+			ch <- packet{"", syn.acknowledgement + 100, syn.sequence + 1, 0, 0}
 			fmt.Println("sending step2")
 		}
 		//Step 3 listen for requests, check x+1 = syn and y+1 = ack
@@ -58,8 +63,17 @@ func server(ch chan packet) {
 			//Step 4 listen for data
 			exSec++
 			exAck = exAck + 100
-			fmt.Print(syn.data + " number: ")
-			fmt.Println(syn.sequence)
+
+			if syn.sequence == 1 {
+				recivedData = make([]string, syn.totalAmount)
+			}
+
+			recivedData[syn.orderNumber] = syn.data
+
+			if syn.sequence == syn.totalAmount {
+				fmt.Printf("%+q", recivedData)
+				break
+			}
 		}
 	}
 }
@@ -69,6 +83,7 @@ type packet struct {
 	sequence        int
 	acknowledgement int
 	orderNumber     int
+	totalAmount     int
 }
 
 func (p packet) Marshalling() []packet {
